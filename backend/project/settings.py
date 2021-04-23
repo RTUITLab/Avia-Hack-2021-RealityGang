e2a9c63db2863cc07ba0090ba6a4681dd0377fef
AlexGeniusMan <alexgeniusman@gmail.com>
2021-04-23 23:24:28 +0300

    test_db added

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'test_db.sqlite3'),
     }
 }
 
