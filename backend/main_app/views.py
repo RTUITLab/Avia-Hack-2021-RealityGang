9d291a66fddf971e2e66fe66a9bbd501546f0ebf
AlexGeniusMan <alexgeniusman@gmail.com>
2021-04-24 17:34:54 +0300

    pagination added to GetMessagesView

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from .serializers import *
 from .models import *
 
@@ -12,11 +14,72 @@ class GetMessagesView(APIView):
 
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
-        messages = Message.objects.filter(user=request.user)
-        messages = MessageSerializer(messages, context={'request': request}, many=True).data
+    # def post(self, request):
+    #     messages = Message.objects.filter(user=request.user)
+    #     messages = MessageSerializer(messages, context={'request': request}, many=True).data
+    #
+    #     return Response(messages)
 
-        return Response(messages)
+    def is_digit(self, find_by_letters):
+        if find_by_letters.isdigit():
+            return True
+        else:
+            try:
+                float(find_by_letters)
+                return True
+            except ValueError:
+                return False
+
+    def post(self, request):
+        # find_by_letters = request.POST['find_by_letters']
+        find_by_letters = ''
+
+        data = []
+        next_page = 1
+        previous_page = 1
+
+        if self.is_digit(find_by_letters):
+
+            messages = Message.objects.filter(
+                Q(user=request.user),
+                Q(pk=find_by_letters) |
+                Q(description__icontains=find_by_letters) |
+                Q(description__icontains=find_by_letters.capitalize()) |
+                Q(description__icontains=find_by_letters.lower()) |
+                Q(description__icontains=find_by_letters.upper())
+            )
+        else:
+            messages = Message.objects.filter(
+                Q(user=request.user),
+                Q(description__icontains=find_by_letters) |
+                Q(description__icontains=find_by_letters.capitalize()) |
+                Q(description__icontains=find_by_letters.lower()) |
+                Q(description__icontains=find_by_letters.upper())
+            )
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(messages, 3)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = MessageSerializer(data, context={'request': request}, many=True)
+
+        if data.has_next():
+            next_page = data.next_page_number()
+        if data.has_previous():
+            previous_page = data.previous_page_number()
+
+        return Response({
+            'messages': serializer.data,
+            'count': paginator.count,
+            'numpages': paginator.num_pages,
+            'nextlink': '/api/recent_news?page=' + str(next_page),
+            'prevlink': '/api/recent_news?page=' + str(previous_page)
+        })
 
 
 class IsAuthenticatedView(APIView):
