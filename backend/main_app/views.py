42f1911fe3f7afdf9fe4bc1f31693f9ba64dcd64
AlexGeniusMan <alexgeniusman@gmail.com>
2021-04-24 17:23:48 +0300

    GetMessagesView added

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,5 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import *
+from .models import *
+
+
+class GetMessagesView(APIView):
+    """
+    Returns messages of current user
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        messages = Message.objects.filter(user=request.user)
+        messages = MessageSerializer(messages, context={'request': request}, many=True).data
+
+        return Response(messages)
 
 
 class IsAuthenticatedView(APIView):
