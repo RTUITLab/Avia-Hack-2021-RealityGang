aff7c327946d9ec26e806dd54c60e3a8444d3c3d
AlexGeniusMan <alexgeniusman@gmail.com>
2021-04-24 16:43:39 +0300

    user model added

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    full_name = models.CharField('Фамилия И.О.', max_length=50)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.full_name
