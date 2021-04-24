f06a8badbe76001499255e292c43ff7a3951e10a
AlexGeniusMan <alexgeniusman@gmail.com>
2021-04-24 17:14:14 +0300

    message model fixed

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -5,19 +5,22 @@ from django.contrib.auth.models import AbstractUser
 class Message(models.Model):
     description = models.TextField('Описание', max_length=256)
 
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, verbose_name='Пользователь',
+                             related_name='messages', null=True, blank=True)
+
     class Meta:
         verbose_name = 'Заявка'
         verbose_name_plural = 'Заявки'
 
     def __str__(self):
-        return self.pk
+        return f'Заявка №{self.pk}'
 
 
 class User(AbstractUser):
     full_name = models.CharField('Фамилия И.О.', max_length=50)
 
-    messages = models.ForeignKey('Message', on_delete=models.SET_NULL, verbose_name='Заявки',
-                                 related_name='user', null=True, blank=True)
+    # messages = models.ForeignKey('Message', on_delete=models.SET_NULL, verbose_name='Заявки',
+    #                              related_name='user', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Пользователь'
