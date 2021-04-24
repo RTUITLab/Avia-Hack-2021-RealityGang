5ba98b8ddfc09037f00527badfda90b9f461d8e5
AlexGeniusMan <alexgeniusman@gmail.com>
2021-04-24 17:25:17 +0300

    MessageSerializer added

---
+++ backend/main_app/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import *
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%d.%m.%Y %H:%M")
+
+    class Meta:
+        depth = 2
+        model = Message
+        fields = ('id', 'description', 'created_at')
